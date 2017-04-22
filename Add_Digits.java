a96a3e780e1dadf50b2524d090f1e27ad3349193
man-inblack <just_yang@yeah.net>
2017-04-22 23:09:24 +0800

    拆分非负整数并向相加，直至结果只有一位数

---
+++ Add_Digits.java
@@ -0,0 +1,11 @@
+
+public class Add_Digits {
+	public int addDigits(int num) {
+		
+		return (num-1)%9+1;
+    }
+	/*public static void main(String[] args){
+		System.out.println(new Add_Digits().addDigits(38));
+		System.out.println(64%9);
+	}*/
+}
