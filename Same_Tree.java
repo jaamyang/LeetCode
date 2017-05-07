a00ca10438e79f979a163dc81887de6ccaab2546
man-inblack <just_yang@yeah.net>
2017-05-07 11:38:12 +0800

    判断两颗二叉树是否完全相同

--- Same_Tree.java
+++ Same_Tree.java
@@ -3,17 +3,19 @@ public class Same_Tree {
 	public boolean isSameTree(TreeNode p, TreeNode q) {
         if(null != p && null != q){
             if(p.val != q.val) return false;
-            if((null == p&& null !=q)||(null != p&& null ==q)) return false;
-            isSameTree(p.left,q.left);
-            isSameTree(p.right,q.right);
+            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
         }
+        if((null == p&& null !=q)||(null != p&& null ==q)) return false;
         return true;
     }
 	public static void main(String[] args){
-		TreeNode p = new TreeNode(2);
-		TreeNode q = new TreeNode(1);
-		/*q.left = new TreeNode(2);
-		p.left = new TreeNode(3);*/
+		TreeNode p = null ;//new TreeNode(1);
+		TreeNode q = null ;//new TreeNode(1);
+		//TreeNode q = null;
+		//q.left = new TreeNode(2);
+		//p.left = new TreeNode(2);
+		//q.right = new TreeNode(1);
+		//p.right = new TreeNode(2);
 		System.out.println(new Same_Tree().isSameTree(p, q));
  	}
 }
