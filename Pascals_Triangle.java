84c7feeceb5e4d7d97820459a697d2b56f30cc5a
man-inblack <just_yang@yeah.net>
2017-05-21 13:42:43 +0800

    求出N阶帕斯卡数列

--- Pascals_Triangle.java
+++ Pascals_Triangle.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pascals_Triangle {
-	public static void main(String[] args){
+	/*public static void main(String[] args){
 		System.out.println(new Pascals_Triangle().generate(0));
-	}
+	}*/
 	public List<List<Integer>> generate(int numRows){
 		List<List<Integer>> al = new ArrayList<List<Integer>>();
 		for(int i=0;i<numRows;i++){
