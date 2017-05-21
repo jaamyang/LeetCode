f8f11997df1a965488774f2562a58fefbca6dda7
man-inblack <just_yang@yeah.net>
2017-05-21 13:37:05 +0800

    给定N枚硬币，求出硬币能够排列成阶梯型的最大阶梯数

---
+++ arrange_coins.java
@@ -0,0 +1,9 @@
+
+public class arrange_coins {
+	public static void main(String[] args){
+		System.out.println(new arrange_coins().arrangeConis(18));
+	}
+	public int arrangeConis(int num){
+		return (int)(-1+Math.sqrt((1+8*num)))/2;
+	}
+}
